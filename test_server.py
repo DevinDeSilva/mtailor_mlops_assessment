40faae2d749f4ed1890133bcd2de5d2f9e6629a6
DevinDeSilva <devin.18@cse.mrt.ac.lk>
2023-03-18 16:30:49 +0530

    updating test server and adding pytorch onnx model

--- test_server.py
+++ test_server.py
@@ -9,14 +9,20 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 model_key = os.getenv("MODEL_KEY")
 
-def run_request(file_path):
-    model_inputs = {"file_path":file_path} # anything you want to send to your model
+def run_request(model_inputs): # anything you want to send to your model
     start_time = time()
     out = banana.run(api_key, model_key, model_inputs)
     end_time = time()
     print(out,f"Time taken :- {end_time-start_time} s")
     
+def run_tests():
+    run_request({"test":True})
+    
+def run_single_request(file_path):
+    run_request({"file_path":file_path})
+    
     
 if __name__ == "__main__":
-    run_request("test_images/n01667114_mud_turtle.JPEG")
+    run_single_request("test_images/n01667114_mud_turtle.JPEG")
+    run_tests()
 
